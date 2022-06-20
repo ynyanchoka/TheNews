bb38bed140b77f6a316e4605738ef425b20b3d86
ynyanchoka <monaryymelda@gmail.com>
2022-06-20 14:41:10 +0300

    add api key

---
+++ app/src/main/java/com/monari/thenews/Constants.java
@@ -0,0 +1,6 @@
+package com.monari.thenews;
+
+public class Constants {
+    public static final String NEWS_BASE_URL = "https://newsapi.org/v2/";
+    public static final String NEWS_API_KEY = BuildConfig.NEWS_API_KEY;
+}
