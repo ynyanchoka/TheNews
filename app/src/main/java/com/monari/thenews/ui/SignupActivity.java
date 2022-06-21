63afa8bd19276aa786914f2794630cd6dae60a1c
ynyanchoka <monaryymelda@gmail.com>
2022-06-21 22:44:19 +0300

    add authentication

--- app/src/main/java/com/monari/thenews/ui/SignupActivity.java
+++ app/src/main/java/com/monari/thenews/ui/SignupActivity.java
@@ -1,14 +1,22 @@
 package com.monari.thenews.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.monari.thenews.MainActivity;
 import com.monari.thenews.R;
+
+import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,6 +37,9 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
     @BindView(R.id.userConfirmPassword)
     EditText mUserConfirmPassword;
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +47,13 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_signup);
         ButterKnife.bind(this);
 
+        mAuth = FirebaseAuth.getInstance();
+        createAuthStateListener();
+
         midSignUpButton.setOnClickListener(this);
         mIdLoginButton.setOnClickListener(this);
     }
+
 
     @Override
     public void onClick(View v){
@@ -52,12 +67,60 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         }
 
         if (v == midSignUpButton) {
-//            createNewUser();
-//            showProgressBar();
+            createNewUser();
         }
 
-
-
-
     }
+
+    private void createNewUser() {
+        final String name = mname.getText().toString().trim();
+        final String email = muserEmail.getText().toString().trim();
+        String password = muserPassword.getText().toString().trim();
+        String confirmPassword = mUserConfirmPassword.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+
+                        Log.d(TAG, "Authentication successful");
+                    } else {
+                        Toast.makeText(SignupActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+
+                });
+    }
+
+    private void createAuthStateListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+
 }
