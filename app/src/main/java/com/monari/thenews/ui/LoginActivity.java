e52d9ad278f7584aad284567783a1d5d88d84b30
ynyanchoka <monaryymelda@gmail.com>
2022-06-21 22:11:38 +0300

    add ability to switch between login and signup activity

--- app/src/main/java/com/monari/thenews/ui/LoginActivity.java
+++ app/src/main/java/com/monari/thenews/ui/LoginActivity.java
@@ -2,15 +2,56 @@ package com.monari.thenews.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.monari.thenews.R;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String TAG = LoginActivity.class.getSimpleName();
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @BindView(R.id.idLoginButton)
+    Button midLoginButton;
+    @BindView(R.id.signUpButton) Button mSignUpButton;
+    @BindView(R.id.idUserEmail)
+    EditText midUserEmail;
+    @BindView(R.id.iduserPassword)
+    EditText midUserPassword;
+    @BindView(R.id.firebaseProgressBar)
+    ProgressBar mSignInProgressBar;
+//    @BindView(R.id.loadingTextView) TextView mLoadingSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        mSignUpButton.setOnClickListener(this);
+        midLoginButton.setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View view) {
+        if (view == midLoginButton) {
+//            loginWithPassword();
+//            showProgressBar();
+        }
+        if (view == mSignUpButton) {
+            Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
     }
 }
--- app/src/main/java/com/monari/thenews/ui/SignupActivity.java
+++ app/src/main/java/com/monari/thenews/ui/SignupActivity.java
@@ -2,15 +2,62 @@ package com.monari.thenews.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.monari.thenews.R;
 
-public class SignupActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String TAG = SignupActivity.class.getSimpleName();
+
+    @BindView(R.id.idSignUpButton)
+    Button midSignUpButton;
+    @BindView(R.id.name)
+    EditText mname;
+    @BindView(R.id.userEmail)
+    EditText muserEmail;
+    @BindView(R.id.userPassword)
+    EditText muserPassword;
+    @BindView(R.id.idLoginButton)
+    Button mIdLoginButton;
+    @BindView(R.id.userConfirmPassword)
+    EditText mUserConfirmPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        ButterKnife.bind(this);
+
+        midSignUpButton.setOnClickListener(this);
+        mIdLoginButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+
+
+        if (v == mIdLoginButton) {
+            Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+
+        if (v == midSignUpButton) {
+//            createNewUser();
+//            showProgressBar();
+        }
+
+
+
+
     }
 }
