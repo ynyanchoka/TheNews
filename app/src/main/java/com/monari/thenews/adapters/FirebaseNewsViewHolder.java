6c616eff3fd77bb235c00450d91efba308769e96
ynyanchoka <monaryymelda@gmail.com>
2022-06-21 20:32:40 +0300

    add ability to save news

---
+++ app/src/main/java/com/monari/thenews/adapters/FirebaseNewsViewHolder.java
@@ -0,0 +1,91 @@
+package com.monari.thenews.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.monari.thenews.Constants;
+import com.monari.thenews.R;
+import com.monari.thenews.models.Article;
+import com.monari.thenews.ui.NewsDetailActivity;
+import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FirebaseNewsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener   {
+
+    View mView;
+    Context mContext;
+
+    public FirebaseNewsViewHolder(View itemView) {
+        super(itemView);
+        mView = itemView;
+        mContext = itemView.getContext();
+        itemView.setOnClickListener(this);
+    }
+
+    public void bindNews(Article article) {
+        ImageView newsImageView = (ImageView) mView.findViewById(R.id.newsImageView);
+//        TextView dateTextView = (TextView) mView.findViewById(R.id.publishDate);
+        TextView sourceTextView = (TextView) mView.findViewById(R.id.newsSource);
+        TextView titleTextView = (TextView) mView.findViewById(R.id.titleNews);
+//        TextView descTextView = (TextView) mView.findViewById(R.id.news_desc);
+//        TextView contentTextView = (TextView) mView.findViewById(R.id.news_content);
+//        TextView readTextView = (TextView) mView.findViewById(R.id.readmore);
+//        ImageButton saveTextView = (ImageButton) mView.findViewById(R.id.save);
+
+        Picasso.get().load(article.getUrlToImage()).into(newsImageView);
+
+
+//        dateTextView.setText(article.getPublishedAt());
+        sourceTextView.setText(article.getSource().getName());
+        titleTextView.setText(article.getTitle());
+//        descTextView.setText(article.getDescription());
+//        contentTextView.setText(article.getContent());
+        titleTextView.setText(article.getTitle());
+        titleTextView.setText(article.getTitle());
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        final ArrayList<Article> articles = new ArrayList<>();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_ARTICLES);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    articles.add(snapshot.getValue(Article.class));
+                }
+
+                int itemPosition = getLayoutPosition();
+
+                Intent intent = new Intent(mContext, NewsDetailActivity.class);
+                intent.putExtra("position", itemPosition + "");
+                intent.putExtra("articles", Parcels.wrap(articles));
+
+                mContext.startActivity(intent);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
+
+
+}
--- app/src/main/java/com/monari/thenews/ui/SavedActivity.java
+++ app/src/main/java/com/monari/thenews/ui/SavedActivity.java
@@ -2,21 +2,54 @@ package com.monari.thenews.ui;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.monari.thenews.Constants;
 import com.monari.thenews.MainActivity;
 import com.monari.thenews.R;
+import com.monari.thenews.adapters.FirebaseNewsViewHolder;
+import com.monari.thenews.models.Article;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SavedActivity extends AppCompatActivity {
+
+    private DatabaseReference mNewsReference;
+    private FirebaseRecyclerAdapter<Article, FirebaseNewsViewHolder> mFirebaseAdapter;
+
+    @BindView(R.id.recyclerView)
+    RecyclerView mRecyclerView;
+    @BindView(R.id.errorTextView)
+    TextView mErrorTextView;
+    @BindView(R.id.progressBar)
+    ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved);
+        ButterKnife.bind(this);
+
+        mNewsReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_ARTICLES);
+        setUpFirebaseAdapter();
+        hideProgressBar();
+        showNews();
 
         // Initialize and assign variable
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
@@ -45,5 +78,51 @@ public class SavedActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    private void setUpFirebaseAdapter(){
+        FirebaseRecyclerOptions<Article> options =
+                new FirebaseRecyclerOptions.Builder<Article>()
+                        .setQuery(mNewsReference, Article.class)
+                        .build();
+
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<Article, FirebaseNewsViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull FirebaseNewsViewHolder firebaseNewsViewHolder, int position, @NonNull Article article) {
+                firebaseNewsViewHolder.bindNews(article);
+            }
+
+            @NonNull
+            @Override
+            public FirebaseNewsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.general_list_item, parent, false);
+                return new FirebaseNewsViewHolder(view);
+            }
+        };
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mFirebaseAdapter!= null) {
+            mFirebaseAdapter.stopListening();
+        }
+    }
+
+    private void showNews() {
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgressBar() {
+        mProgressBar.setVisibility(View.GONE);
     }
 }
